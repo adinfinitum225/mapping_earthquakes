72273062003652bfc57d6c8ba6f0a413670757af
Zach Wells <zach.13.wells@gmail.com>
2022-03-21 15:55:09 -0500

    Changed line color and weight, added popups

--- mapping_geoJSON_linestrings/static/js/logic.js
+++ mapping_geoJSON_linestrings/static/js/logic.js
@@ -30,7 +30,7 @@ let baseMaps = {
 let map = L.map('mapid', {
     center: [44.0, -80.0],
     zoom: 2,
-    layers: [light]
+    layers: [dark]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -39,9 +39,20 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the toronto GeoJSON URL
 let torontoData = "https://raw.githubusercontent.com/adinfinitum225/mapping_earthquakes/mapping_geoJSON_linestrings/mapping_geoJSON_linestrings/torontoRoutes.json";
 
+// Create a style for the lines
+let myStyle = {
+		color: "#ffffa1",
+		weight: 2
+};
+
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+	    style: myStyle,
+  		onEachFeature: function(feature, layer) {
+  			layer.bindPopup("<h3>Airline: " + feature.properties.airline + "</h3><hr><h3>Destination: " + feature.properties.dst + "</h3>");
+  }
+  }).addTo(map);
 });
